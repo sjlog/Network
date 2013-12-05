33876634b048865b06148cc5830bcae68ce98115
sangjo_itwill <sangjo_itwill@local.local>
2013-12-05 11:24:55 +0900

    Initial Commit

---
+++ Network/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Network
+//
+//  Created by sangjo_itwill on 2013. 12. 5..
+//  Copyright (c) 2013년 sj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
